809168e5c0b1c7f671f2a86a4c169ab4883fc690
Rita-cedcoss <ritapal@cedcoss.com>
2023-02-23 17:58:54 +0530

    string generation task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import StringGeneration from './string_generation/StringGeneration';
+import StringGeneration from './components/StringGeneration';
 
 function App() {
   return (
